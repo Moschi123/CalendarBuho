0ca85280ca4c9822c6f13ce21c8170dec2be7ab5
moschi123 <francoeduardo500@gmail.com>
2023-07-01 17:22:35 -0300

    mesaji

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['date-fns/locale/es'], // Aquí se especifican los módulos que no se deben incluir en el paquete
+    },
+  },
+});
