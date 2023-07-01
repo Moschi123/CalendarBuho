67c2d91338a1a51c4a1948d1f635c4a4314e0d0f
moschi123 <francoeduardo500@gmail.com>
2023-07-01 16:41:56 -0300

    arreglando las dates

--- src/helpers/calendarLocalizer.js
+++ src/helpers/calendarLocalizer.js
@@ -1,20 +1,15 @@
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
-import esEs from 'date-fns/locale/ES'
-import { dateFnsLocalizer } from 'react-big-calendar'
-
+import { dateFnsLocalizer } from 'react-big-calendar';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
+import esES from 'date-fns/locale/es';
 
 const locales = {
-    'es': esEs,
-  }
+  'es': esES,
+}
 
-  
 export const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-  });
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
